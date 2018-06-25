29d14d3d50b030d2e4ae60b8e233b59dea9a9e32
Hudson Pereira <hudson.byte@gmail.com>
2018-06-25 11:30:39 -0300

    pass book uri using intent

--- app/src/main/java/com/example/hudson/bookstore/EditorActivity.java
+++ app/src/main/java/com/example/hudson/bookstore/EditorActivity.java
@@ -1,5 +1,7 @@
 package com.example.hudson.bookstore;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,14 @@ public class EditorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
+
+        Intent intent = getIntent();
+        Uri bookUri = intent.getData();
+
+        if (bookUri != null) {
+            getSupportActionBar().setTitle(R.string.edit_book);
+        } else {
+            getSupportActionBar().setTitle(R.string.add_book);
+        }
     }
 }
--- app/src/main/java/com/example/hudson/bookstore/MainActivity.java
+++ app/src/main/java/com/example/hudson/bookstore/MainActivity.java
@@ -1,15 +1,19 @@
 package com.example.hudson.bookstore;
 
 import android.app.LoaderManager;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.CursorLoader;
+import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -40,19 +44,31 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         booksListView.setEmptyView(findViewById(R.id.empty_text_view));
         booksListView.setAdapter(bookCursorAdapter);
 
+        booksListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, EditorActivity.class);
+
+                intent.setData(ContentUris.withAppendedId(BookContract.CONTENT_URI, id));
+
+                startActivity(intent);
+            }
+        });
+
         getLoaderManager().initLoader(CURSOR_LOADER, null, this);
 
         insertButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ContentValues cv = new ContentValues();
-                cv.put(BookEntry.COLUMN_PRODUCT_NAME, getString(R.string.demo_product_name));
-                cv.put(BookEntry.COLUMN_PRICE, DEMO_BOOK_PRICE);
-                cv.put(BookEntry.COLUMN_QUANTITY, DEMO_BOOK_QUANTITY);
-                cv.put(BookEntry.COLUMN_SUPPLIER_NAME, getString(R.string.demo_supplier_name));
-                cv.put(BookEntry.COLUMN_SUPPLIER_PHONE_NUMBER, getString(R.string.demo_supplier_phone));
-
-                getContentResolver().insert(BookContract.CONTENT_URI, cv);
+                startActivity(new Intent(MainActivity.this, EditorActivity.class));
+//                ContentValues cv = new ContentValues();
+//                cv.put(BookEntry.COLUMN_PRODUCT_NAME, getString(R.string.demo_product_name));
+//                cv.put(BookEntry.COLUMN_PRICE, DEMO_BOOK_PRICE);
+//                cv.put(BookEntry.COLUMN_QUANTITY, DEMO_BOOK_QUANTITY);
+//                cv.put(BookEntry.COLUMN_SUPPLIER_NAME, getString(R.string.demo_supplier_name));
+//                cv.put(BookEntry.COLUMN_SUPPLIER_PHONE_NUMBER, getString(R.string.demo_supplier_phone));
+//
+//                getContentResolver().insert(BookContract.CONTENT_URI, cv);
             }
         });
     }
